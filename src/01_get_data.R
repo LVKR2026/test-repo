29ec2b58e869a1a20b329e04d5d543bfd192cdba
LVKR2026 <lbruhn@hotmail.com>
2026-05-28 14:29:11 +0000

    fjerner Kuwait, da outlier i 1952

--- src/01_get_data.R
+++ src/01_get_data.R
@@ -1,3 +1,11 @@
 library(tidyverse)
 d <- read_csv('https://tinyurl.com/gapcsv')
 write_csv(d,'data/gapminder.csv')
+
+#På plot gdpPercap vs. lifeExp i 1952 ses en outlier, det undersøges at Kuwait er ouelier og denne sorteres fra:
+d <- read_csv("data/gapminder.csv")
+d_clean <- d %>%
+  filter(!(country == "Kuwait"))
+
+# Gem den rensede version
+write_csv(d_clean, "data/gapminder.csv")
